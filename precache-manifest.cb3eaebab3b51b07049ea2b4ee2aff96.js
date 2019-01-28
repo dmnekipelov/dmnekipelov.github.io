1bb0a91c7bd763cd3fad7d509b7d3eb4c099427b
jambotron <nekipelov2@gmail.com>
2019-01-28 18:26:16 +0200

    Init

---
+++ precache-manifest.cb3eaebab3b51b07049ea2b4ee2aff96.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cfded5f45ffa8fb8690e",
+    "url": "/static/js/main.cfded5f4.chunk.js"
+  },
+  {
+    "revision": "c6b25d29283155750d4e",
+    "url": "/static/js/1.c6b25d29.chunk.js"
+  },
+  {
+    "revision": "cfded5f45ffa8fb8690e",
+    "url": "/static/css/main.cee4746e.chunk.css"
+  },
+  {
+    "revision": "c6b25d29283155750d4e",
+    "url": "/static/css/1.958b2d5d.chunk.css"
+  },
+  {
+    "revision": "6f5637c7ef2c85ca835ddc2b715c5214",
+    "url": "/index.html"
+  }
+];
